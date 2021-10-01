ed402bacd7352072e3233ea2c0c10459affc12a3
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-01 14:52:44 +0530

    Update Buzz_number.java

--- Buzz_number.java
+++ Buzz_number.java
@@ -7,14 +7,17 @@ public class Buzz_number {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
 		System.out.println("Enter a number"); // Telling the user to input a number
 		int num = scan.nextInt( ); // Input function
+		
 		if(num%7==0 || num%10==7) { 
 			System.out.println(num + " is a buzz number"); // If num is dvisible by 7 or if it ends with 7
 		}
 		else {
 			System.out.println(num + " is not a buzz number"); // If num is not dvisible by 7 or if it doesnt ends with 7
 		}
+		
 		scan.close( ); // closing the scanner class since we have no further use of it
 	}
 }
