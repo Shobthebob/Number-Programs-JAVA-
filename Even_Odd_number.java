b277263bb71a6047b49cffe561409e9775bb9385
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-01 19:32:57 +0530

    Update Even_Odd_number.java

--- Even_Odd_number.java
+++ Even_Odd_number.java
@@ -4,19 +4,19 @@ public class Even_Odd_number {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
 		
-		System.out.println("Enter a number");
-		int num = scan.nextInt( );
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int num = scan.nextInt( ); // Input function
 		
-		if(num%2==0) {
+		if(num%2==0) { // If num is divisible by 2
 			System.out.println(num + " is an even number");
 		}
-		else {
+		else { // If num is not divisible by 2
 			System.out.println(num + " is an odd number");
 		}
 		
-		scan.close( );	
+		scan.close( );	// Closing the scanner class because we have no further use of it 
 
 	}
 
