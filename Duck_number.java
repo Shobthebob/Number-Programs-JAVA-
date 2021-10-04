91fda161b7e73749850fe364ca7d2789d48aa05b
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-04 21:41:04 +0530

    Update Duck_number.java

--- Duck_number.java
+++ Duck_number.java
@@ -1,39 +1,39 @@
 /*A number that has at least one 0 ( but not at the beginning of the number ) is called a duck
- * number.
+ * number. [Examples - 102, 23230, 230982098231]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Duck_number {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-		String num = scan.next( );
-		int len = num.length( );
-		char ch;
-		int c = 0;
-		for(int i=0; i<len; i++) {
-			ch = num.charAt(i);
-			if(num.charAt(0)=='0') {
-				for(int j=0; j<(len-1); j++) {
-					if((num.substring(1)).charAt(j) == '0') {
-						c = 1;
-					}
-				}				
-			}
-			else if(ch == '0') {
-				c = 1;
-			}
+
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		String num = scan.next( ); // Input function
+		int len = num.length( ); // Length of the number entered     
+		int count = 0; // Will count the number of '0' in the number entered
+    		String n = String.valueOf(num); // Converts the number entred to a string type
+
+   		if(n.charAt(0)=='0'){
+      			System.out.println(num + " is not a duck number"); // if the 1st digit of the number entered is a '0'
+      			System.exit(0); // To terminate the entire program
 		}
-		if(c==1) {
-			System.out.println(num + " is a duck number");			
+   	 	else{
+      			for(int i=0; i<len; i++){ 
+        			if(n.charAt(i)=='0'){ // To count the number of '0'
+          				count++; // increments by 1 is a 0 is found
+        			}
+     			}
+    		}
+    
+		if(count>=1) {
+			System.out.println(num + " is a duck number"); // If 1 or more '0' is found			
 		}
 		else {
-			System.out.println(num + " is not a duck number");
+			System.out.println(num + " is not a duck number"); // if there is no '0' in the number
 		}
-		scan.close( );
+		scan.close( ); // closing the scanner class since we have no further use of it
 	}
-
 }
