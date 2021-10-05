0e33666aeecf588d12c87a9ec760716bcc586dfb
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-05 18:29:18 +0530

    Update Evil_number.java

--- Evil_number.java
+++ Evil_number.java
@@ -1,35 +1,37 @@
-/*An evil number is a positive whole  number that has an even number of 1’s in its binary 
- * equivalent. 
+/*An evil number is a positive whole number that has an even number of 1â€™s in its binary 
+ * equivalent. [Examples - 3, 12, 20]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Evil_number {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-	    int n = scan.nextInt( );
-		String num = Long.toBinaryString(n);
-		char ch;
-		int count = 0;
-		int len = num.length( );
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int n = scan.nextInt( ); // Input function
+		String num = Long.toBinaryString(n); // To convert the entered number to binary 
+		int count = 0; // It'll store the number of 1s in the binary number
+		int len = num.length( ); // Length of the number
+		
 		for(int i=0; i<len; i++) {
-			ch = num.charAt(i);
+			char ch = num.charAt(i); // To iterate through every digit in the number
 			if(ch == '1') {
-				count ++;
+				count ++; // Counts the number of 1s found
 			}
 		}
-		System.out.println("The binary value of " + n + " is " + num);
-		System.out.println("");
+		
+		System.out.println("The binary value of " + n + " is " + num); // Printing the binary value of the entred number
+		System.out.println(""); // To make the output neater
+		
 		if(count%2==0) {
-			System.out.println(n + " is an evil number");
+			System.out.println(n + " is an evil number"); // If even number of 1s are present 
 		}
 		else {
-			System.out.println(n + " is not a evil number");
+			System.out.println(n + " is not a evil number"); // If even number of 1s are not present 
 		}
-		scan.close( );
+		scan.close( ); // closing the scanner class since we have no further use of it
 	}
-
 }
