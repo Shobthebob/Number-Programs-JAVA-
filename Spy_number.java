7fd67e5945b3e018da859259b974bde7e7514369
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-18 03:28:42 +0530

    Update Spy_number.java

--- Spy_number.java
+++ Spy_number.java
@@ -1,30 +1,34 @@
 /* If the sum of the digits of a number is equal to the product of the digits of the number then 
- * the number is said to be a spy number
+ * the number is said to be a spy number. [Examples - 1, 123, 1124]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Spy_number {
 
 	public static void main(String[] args) {	
-		Scanner scan = new Scanner (System.in);
-		int product=1,sum=0;
-        int ld;        
-        System.out.println("Enter a number:");
-        int n = scan.nextInt( );
-        while(n>0)
-        {
-            ld=n%10;
-            sum=sum+ld;
-            product=product*ld;
-            n=n/10;
-        }        
-        if(sum==product)
-            System.out.println("Given number is spy number");
-        else
-            System.out.println("Given number is not spy number");
-        scan.close( );
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int n = scan.nextInt( ); // Input function
+		int product=1,sum=0; // Initializing and Declaring product and sum with integer values 1 and 0
+        	int ld; // Initializing ld  
+		
+        	while(n>0) { // runnning a while loop until n ends up being 0
+            		ld=n%10; // ld will store the last digit of the number
+            		sum=sum+ld; // sum adds every digit (ld)
+            		product=product*ld; // product multiplies every digit (ld)
+            		n=n/10; // diving n by 10 to remove the last digit and avoid performing any further actions on it
+        	}        
+		
+        	if(sum==product){
+            		System.out.println("Given number is spy number"); // if the sum and the product of each digit are equal
+		}
+        	else{
+            		System.out.println("Given number is not spy number"); // if the sum and the product of each digit are not equal
+		}
+		
+        	scan.close( ); // closing the scanner class since we have no further use of it
 	}
-
 }
