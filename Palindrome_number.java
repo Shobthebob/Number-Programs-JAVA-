0cf6fcb004f984ae2027a55315a5d66e7ebc73e0
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-18 02:15:32 +0530

    Update Palindrome_number.java

--- Palindrome_number.java
+++ Palindrome_number.java
@@ -1,32 +1,35 @@
 /* If the reverse of the number is equal to the number itself then the number is said to be a 
- *  Palindrome number
+ *  Palindrome number. [Examples - 101, 23432, 32409832423890423]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Palindrome_number {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int num = scan.nextInt( );
-		String s = "";
-		int d = 0;
-        int e = num;
-        while(num!=0) {
-        	d = num%10;
-        	s+=d;
-        	num = num/10;
-        }
-        int rev = Integer.parseInt(s);
-        if(rev==e) {
-        	System.out.println(e + " is a palindrome number");        	
-        }        
-        else {
-        	System.out.println(e + " is not a palindrome number");
-        }
-        scan.close( );
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int num = scan.nextInt( ); // Input function
+		String rev = "";
+		int rem = 0;
+        	int e = num; // By storing the value of num in e, we'll be abe to use num later without altering its value
+		
+        	while(e!=0) {
+        		rem = e%10; // rem will store the last value of the number
+        		rev+=rem; // rev will store the string value of the last digit being the first digit and so on (in short the reverse)
+        		e = e/10; // diving e by 10 to remove the last digit and avoid performing any further actions on it
+        	}
+		
+        	if(Integer.parseInt(rev)==num) { // Integer.parseInt( ) converts the string value to an integer type
+        		System.out.println(num + " is a palindrome number"); // if the reverse of the number is equal to the original number         	
+        	}        
+        	else {
+        		System.out.println(num + " is not a palindrome number"); // if the reverse of the number is not equal to the original number  
+        	}
+		
+        	scan.close( ); // closing the scanner class since we have no further use of it
 	}
 
 }
