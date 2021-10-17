fb3581d9b5568ecc90735da98abacc3b0a7719c2
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-18 03:21:12 +0530

    Update Pronic_number.java

--- Pronic_number.java
+++ Pronic_number.java
@@ -1,27 +1,34 @@
-/* The product of two consecutive integers is said to be a Pronic number */
+/* The product of two consecutive integers is said to be a Pronic number 
+* [Examples - 30, 42, 56]
+*/
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Pronic_number {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int num = scan.nextInt( );	
-		int a=0;
-		for(int i = 0; i<=num; i++) {		
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int num = scan.nextInt( ); // Input function
+		int a=0; // Initializing and Declaring a with an integer value 0 (representing false)
+		
+		for(int i=0; i<=num; i++) { // Running a for loop till num		
 			if((i*(i+1)) == num) {
-				a = 1; 
+				a = 1; // a turns 1(representing true) when it finds out that num is a product of two
+					// consecutive integers
 			}			
 		}
+		
 		if(a==1) {
-			System.out.println(num + " is a pronic number");		 
+			System.out.println(num + " is a pronic number"); // if a came out to be 1		 
 		}
 		else {
-			System.out.println(num + " is not a pronic number");
+			System.out.println(num + " is not a pronic number"); // if a stayed with the number 0
 		}
-		scan.close( );
+		
+		scan.close( ); // closing the scanner class since we have no further use of it
 	}
 
 }
