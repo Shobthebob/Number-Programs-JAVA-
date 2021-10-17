fed4a48ea6960d61e95afe3a327c0a54672b17a5
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-18 02:52:16 +0530

    Update Perfect_number.java

--- Perfect_number.java
+++ Perfect_number.java
@@ -1,31 +1,31 @@
 /* If the sum of the factors of the number is equal to the number itself then the number is said
- * to be a Perfecr number
+ * to be a Perfect number. [Examples - 6, 28, 496]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class 
 
-import java.util.*;
 public class Perfect_number {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int num = scan.nextInt( );
-		int e = 0;
-		for(int i = 1; i<=num; i++) {
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int num = scan.nextInt( ); // Input function
+		int e = 0; // Initializing and Declaring e with integer value 0
+		for(int i = 1; i<=num; i++) { // running a for loop till num
 			if(num%i==0) {
-				i+=i;				
+				i+=i; // going to hold the sum of the factors			
 			}
-			e = i;
+			e = i; // e stores the sum of the factors
 		}
 		if(num == e) {
-			System.out.println(num + " is a perfect number");
+			System.out.println(num + " is a perfect number"); // if the sum of the factors is equal to the number
 		}
 		else {
-			System.out.println(num + " is not a perfect number");
+			System.out.println(num + " is not a perfect number"); // if the sum of the factors is not equal to the number
 		}
-		scan.close( );
-
+		
+		scan.close( ); // closing the scanner class since we have no further use of it
 	}
-
 }
