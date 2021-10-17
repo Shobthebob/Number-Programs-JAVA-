67442e15838f8c033eed68152b2e3434aac95321
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2021-10-18 00:10:58 +0530

    Update Magic_number.java

--- Magic_number.java
+++ Magic_number.java
@@ -2,25 +2,44 @@
  * digit recursively by adding the sum of the digits after every addition.
  * If the single-digit comes out to be 1, then the number is a magic number.
  * OR IN SIMPLE FUCKING WORDS
- * If the number, when divided by 9, leave a remainder of 1, the number is a magic number
+ * If the number, when divided by 9, leave a remainder of 1, the number is a magic number.
+ * [Examples - 46, 226, 1234]
  */
 
-package special_numbers;
+import java.util.*; // For the scanner class
 
-import java.util.*;
 public class Magic_number {
-		
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int num = scan.nextInt( );	
-		if(num%9==1) {
-			System.out.println(num + " is a magic number");
+	
+	public static int Magic(int n) { // Ths function is gonna recursively add the sum of digits of the number until it's a single digit
+		int rem=0, sum=0; 
+		while(n!=0) { // loop to store the value of the sum of the digits of the number passed through arguments
+			rem = n%10; // rem will store the last value of the number 
+			sum+=rem; // sum will store the sum of the digits of the number passed through arguments
+			n = n/10; // diving n by 10 to remove the last digit and avoid performing any further actions on it
+		}
+		if(String.valueOf(sum).length( )==1) {
+			return sum; // returns the sum only if a single digit remains
 		}
 		else {
-			System.out.println(num + " is not a magic number");
+			return Magic(sum); // fucntion calls itself if the sum isnt single digit
 		}
-		scan.close( );
+	}
+		
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in); // Creating an object with the name "scan"
+		
+		System.out.println("Enter a number"); // Telling the user to input a number
+		int num = scan.nextInt( ); // Input function
+		
+		if(Magic(num)==1) {
+			System.out.println(num + " is a Magic number"); // if the sum recursively ended up being 1
+		}
+		else {
+			System.out.println(num + " is not a Magic number"); // if the sum recursively didn't end up being 1
+		}
+		
+		scan.close( ); // closing the scanner class since we have no further use of it
 	}
 	
 }
